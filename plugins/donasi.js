3e71b708ae9a24b88c14a37d940c988c094a6fc9
LexusBot19 <77090280+LexusBot19@users.noreply.github.com>
2021-01-26 16:18:51 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Smartfren [088235435804]
-│ • Telkomsel [081357302007]
-│ • Gopay [081357302007]
+╭─「 Donasi • LexusBOT 」
+│ • Pulsa [+62 821-2432-8193]
+│ • Saweria [https://saweria.co/donate/LexusCH]
+│ • Trakteer [https://trakteer.id/LexusCH]
 ╰────
 ╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6281357302007
+│ > Ingin donasi? Wa.me/6281224328193
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
